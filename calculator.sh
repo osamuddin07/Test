e06f4a141abd9d7a55092cb30732b23b0407735b
osamuddin07 <mohdosamuddin@gmail.com>
2026-07-24 14:34:37 +0000

    calculator.sh

---
+++ calculator.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+echo "Enter your First Number: "
+read Fnum
+echo "Enter your Second Number: "
+read Snum
+
+
+echo "Addition: $((Fnum + Snum))"
+echo "Subtrations: $((Fnum - Snum))"
+echo "multipilation: $((Fnum * Snum))"
+echo "division: $((Fnum / Snum))"
+echo "moules: $((Fnum % Snum))"
+
